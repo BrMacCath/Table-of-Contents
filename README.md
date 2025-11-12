a328e690aadf773cb03eb388e65b93107ae520f4
BrMacCath <brian.collery@colostate.edu>
2025-11-12 19:46:42 +0000

    Pushes table back if the smallest title is not 1

--- README.md
+++ README.md
@@ -48,6 +48,7 @@ This project uses [semver](http://semver.org/).
 
 | version | Date        | Notes                    |
 | ------- | ----------  | ------------------------ |
+| `2.5.1` | 2025-11-12  | If no headings start with #, push the table of contents back|
 | `2.5.0` | 2025-11-12  | Allows altered indents in the table of contents  |
 | `2.4.4 `| 2025-11-12  | Can remove codeBlocks from toc |
 | `2.4.3` | 2025-11-09  | Allow manual changes of title |
--- src/markdownFunctions/createSubheadingIndex.ts
+++ src/markdownFunctions/createSubheadingIndex.ts
@@ -9,10 +9,16 @@ export function createSubheadingIndex(
     
     
     let maxDepth = 0;
+    // minDepth is to push the table of contents back a bit
+    // if we don't have any # headings but if every heading
+    // has multiple hashtags.
+    let minDepth = content.indexOf(" ")
+
     let tempContent:[number,string][] =[]
     content.split("\n").forEach((line) =>{
         const spaceIndex = line.indexOf(" ");
         if(spaceIndex>maxDepth){maxDepth = spaceIndex}
+        if(spaceIndex<minDepth){minDepth = spaceIndex}
        tempContent.push([spaceIndex,line.slice(spaceIndex).trim()])
     } )
     let indexNumArray = new Array(maxDepth).fill(0);
@@ -21,7 +27,7 @@ export function createSubheadingIndex(
         const headingDepth= headingContent[0];
         const headingTitle = headingContent[1];
         const headingSignifier = "#".repeat(headingDepth);
-        const tabIndent = "\t".repeat(headingDepth-1);
+        const tabIndent = "\t".repeat(headingDepth-minDepth);
         const headingNum = indexNumArray[headingDepth-1]+1;
         indexNumArray[headingDepth-1] += 1;
         for(let i=headingDepth;i<maxDepth;i++){
